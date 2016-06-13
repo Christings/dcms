ae08070461f5749746bc6a88d33c1a873a8a932c
tianjx <tianstyle@163.com>
2016-06-13 22:27:50 +0800

    替换hibernate为mybatis框架

--- src/main/java/com/web/service/UserService.java
+++ src/main/java/com/web/service/UserService.java
@@ -2,7 +2,25 @@ package com.web.service;
 
 import com.web.entity.User;
 
-public interface UserService {
+import java.util.List;
 
-	public User getUserByName(String userName);
+public interface UserService {
+	/*根据ID查询User*/
+	public User getUserById(int id);
+
+	/*根据name查询User*/
+	public User getUserByName(String name);
+
+	/*获取全部用户*/
+	public List<User> getAllUsers();
+
+	/**
+	 * @param user
+	 * 保存用户
+	 * */
+	public int saveUser(User user);
+
+	public int updateUser(User user);
+
+	public int deleteUser(int id);
 }
