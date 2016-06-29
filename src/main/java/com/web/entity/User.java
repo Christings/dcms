3ed4c0fe0f32b1e2cec86745fdf8cbfceb7fb9bb
tong_yunpeng <tong_yunpeng@PC11381.Hoperun.com>
2016-06-29 11:41:30 +0800

    用户实体类修改

--- src/main/java/com/web/entity/User.java
+++ src/main/java/com/web/entity/User.java
@@ -1,119 +1,112 @@
 package com.web.entity;
 
-import org.apache.ibatis.type.Alias;
-
 import java.io.Serializable;
 import java.util.Date;
 
-/**
- * 系统用户表
- * 
- * @author GZH
- */
 
-@Alias("USER")
+/**
+ * 用户实体类
+* @ClassName: User 
+* @Description: TODO
+* @author 童云鹏 
+* @date 2016年6月29日 上午10:26:23 
+*/
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private String id;
-	private String username;
-	private String password;
-	private String ip;
-	private String telphone;
-	private String email;
-	private Date createTime;
-	private Date lastLoginTime;
-	private Integer level;
-	private String extra;
-	private Integer status;
-
+	private String id;//主键ID
+	private String userName;//用户姓名
+	private String account;//登录账号
+	private String password;//登录密码
+	private String identificationNo;//身份证号
+	private String mobile;//手机号
+	private String sex;//性别
+	private String enabled;//是否启用：0 启用 ，1是不启用
+	private String creatorId;// 创建人ID
+	private String createTime;//创建时间
+	private String remark;//备注
+	private String deleted;//是否逻辑删除：0 未删除 ，1 逻辑删除
+	private String superAdmin;//是否超级管理员 ：0 不是，1 是
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
-	public String getUsername() {
-		return username;
+	public String getUserName() {
+		return userName;
 	}
-
-	public void setUsername(String username) {
-		this.username = username;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
-
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public String getIp() {
-		return ip;
+	public String getIdentificationNo() {
+		return identificationNo;
 	}
-
-	public void setIp(String ip) {
-		this.ip = ip;
+	public void setIdentificationNo(String identificationNo) {
+		this.identificationNo = identificationNo;
 	}
-
-	public String getTelphone() {
-		return telphone;
+	public String getMobile() {
+		return mobile;
 	}
-
-	public void setTelphone(String telphone) {
-		this.telphone = telphone;
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
 	}
-
-	public String getEmail() {
-		return email;
+	public String getSex() {
+		return sex;
 	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public void setSex(String sex) {
+		this.sex = sex;
 	}
-
-	public Date getCreateTime() {
+	public String getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+	public String getCreatorId() {
+		return creatorId;
+	}
+	public void setCreatorId(String creatorId) {
+		this.creatorId = creatorId;
+	}
+	public String getCreateTime() {
 		return createTime;
 	}
-
-	public void setCreateTime(Date createTime) {
+	public void setCreateTime(String createTime) {
 		this.createTime = createTime;
 	}
-
-	public Date getLastLoginTime() {
-		return lastLoginTime;
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getDeleted() {
+		return deleted;
+	}
+	public void setDeleted(String deleted) {
+		this.deleted = deleted;
+	}
+	public String getSuperAdmin() {
+		return superAdmin;
+	}
+	public void setSuperAdmin(String superAdmin) {
+		this.superAdmin = superAdmin;
 	}
 
-	public void setLastLoginTime(Date lastLoginTime) {
-		this.lastLoginTime = lastLoginTime;
-	}
-
-	public Integer getLevel() {
-		return level;
-	}
-
-	public void setLevel(Integer level) {
-		this.level = level;
-	}
-
-	public String getExtra() {
-		return extra;
-	}
-
-	public void setExtra(String extra) {
-		this.extra = extra;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
+	
 
 }
