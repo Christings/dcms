5d2b57c8cd06f56edea8ff209ff504c3854284e2
tong_yunpeng <tong_yunpeng@PC11381.Hoperun.com>
2016-07-05 10:07:35 +0800

    用户管理完善

--- src/main/java/com/web/action/UserController.java
+++ src/main/java/com/web/action/UserController.java
@@ -18,11 +18,30 @@ import com.web.entity.User;
 import com.web.util.AllResult;
 import com.web.util.MD5;
 import com.web.util.UUIDGenerator;
+/**
+ * 用户管理Controller
+* @ClassName: UserController 
+* @Description: TODO
+* @author 童云鹏 
+* @date 2016年7月5日 上午9:58:51
+ */
 @Controller
 @RequestMapping("/user")
 public class UserController extends BaseController{
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 	
+	/**
+	 * 新增用户
+	* @Title: addUser 
+	* @Description: TODO
+	* @param @param user
+	* @param @param request
+	* @param @return   参数 
+	* @return Object    返回类型 
+	* @throws 
+	* @author  童云鹏
+	* @date 2016年7月5日 上午9:59:16
+	 */
 	@RequestMapping(value="/addUser",method=RequestMethod.POST)
 	@ResponseBody
 	public Object addUser(@RequestBody User user, HttpServletRequest request){
@@ -53,10 +72,117 @@ public class UserController extends BaseController{
 		}
 		
 	}
+	/**
+	 * 修改用户
+	* @Title: updateUser 
+	* @Description: TODO
+	* @param @param user
+	* @param @param request
+	* @param @return   参数 
+	* @return Object    返回类型 
+	* @throws 
+	* @author  童云鹏
+	* @date 2016年7月5日 上午9:59:38
+	 */
 	@RequestMapping(value="/updateUser",method=RequestMethod.POST)
 	@ResponseBody
-	public void updateUser(@RequestBody User user,HttpServletRequest request){
-		
+	public Object updateUser(@RequestBody User user,HttpServletRequest request){
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("request param: [menu: {}]", JSON.toJSONString(user));
+		}
+		//TODO 需要添加判断
+		if(StringUtils.isEmpty(user.getAccount())){
+			return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请求参数异常:account用户账号不能为空");
+		}else if(StringUtils.isEmpty(user.getPassword())){
+			return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请求参数异常:password密码不能为空");
+		}else if(StringUtils.isEmpty(user.getUserName())){
+			return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请求参数异常:userName用户名不能为空");
+		}else if(StringUtils.isEmpty(user.getId())){
+			return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请求参数异常:userId不能为空");
+		}
+
+		try {
+			//user.setId(UUIDGenerator.generatorRandomUUID());
+			//user.setPassword(MD5.MD5Encode(user.getPassword()));
+			int result=userService.updateUser(user);
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("save result: {}", result);
+			}
+			return AllResult.okJSON(user);
+		} catch (Exception e) {
+			LOGGER.error("save User fail:", e.getMessage());
+			return AllResult.buildJSON(HttpStatus.INTERNAL_SERVER_ERROR.value(), "系统内部错误,添加菜单失败") ;
+		}
 	}
+	
+	/**
+	 * 
+	* @Title: updateUserEnabled 
+	* @Description: 设置用户是否启用
+	* @param @param user
+	* @param @param request
+	* @param @return   参数 
+	* @return Object    返回类型 
+	* @throws 
+	* @author  童云鹏
+	* @date 2016年7月5日 上午10:04:40
+	 */
+	@RequestMapping(value="/updateUserEnabled",method=RequestMethod.POST)
+	@ResponseBody
+	public Object updateUserEnabled(@RequestBody User user,HttpServletRequest request){
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("request param: [menu: {}]", JSON.toJSONString(user));
+		}
+		//TODO 需要添加判断
+		 if(StringUtils.isEmpty(user.getId())){
+			return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请求参数异常:userId不能为空");
+		}
+
+		try {
+			//user.setId(UUIDGenerator.generatorRandomUUID());
+			//user.setPassword(MD5.MD5Encode(user.getPassword()));
+			userService.updateUserDelete(user.getEnabled(), user.getId());
+			return AllResult.okJSON(user);
+		} catch (Exception e) {
+			LOGGER.error("updateUserEnabled User fail:", e.getMessage());
+			return AllResult.buildJSON(HttpStatus.INTERNAL_SERVER_ERROR.value(), "系统内部错误,添加菜单失败") ;
+		}
+	}
+	
+	/**
+	 * 
+	* @Title: updateUserDelete 
+	* @Description: 用户逻辑删除
+	* @param @param user
+	* @param @param request
+	* @param @return   参数 
+	* @return Object    返回类型 
+	* @throws 
+	* @author  童云鹏
+	* @date 2016年7月5日 上午10:05:35
+	 */
+	@RequestMapping(value="/updateUserDelete",method=RequestMethod.POST)
+	@ResponseBody
+	public Object updateUserDelete(@RequestBody User user,HttpServletRequest request){
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("request param: [menu: {}]", JSON.toJSONString(user));
+		}
+		//TODO 需要添加判断
+		if(StringUtils.isEmpty(user.getId())){
+			return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请求参数异常:userId不能为空");
+		}
+
+		try {
+			//user.setId(UUIDGenerator.generatorRandomUUID());
+			//user.setPassword(MD5.MD5Encode(user.getPassword()));
+			userService.updateUserDelete(user.getDeleted(), user.getId());
+			
+			return AllResult.okJSON(user);
+		} catch (Exception e) {
+			LOGGER.error("delete User fail:", e.getMessage());
+			return AllResult.buildJSON(HttpStatus.INTERNAL_SERVER_ERROR.value(), "系统内部错误,添加菜单失败") ;
+		}
+	}
+	
 
 }
--- src/main/java/com/web/service/UserService.java
+++ src/main/java/com/web/service/UserService.java
@@ -5,6 +5,8 @@ import com.web.entity.User;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface UserService {
 	/*根据ID查询User*/
 	public User getUserById(String id);
@@ -14,6 +16,10 @@ public interface UserService {
 
 	/*获取全部用户*/
 	public List<User> getAllUsers();
+	/*是否启用用户*/
+    public void updateUserEnabled(Integer enabled,String id)throws Exception;
+    /*是否逻辑删除*/
+    public void updateUserDelete(Integer deleted,String id)throws Exception;
 
 	/**
 	 * @param user
