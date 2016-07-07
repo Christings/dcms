45c05d9de8af2fefacc959c5c518efad6a93e6af
tianjx <tianstyle@163.com>
2016-07-08 01:05:40 +0800

    代码优化

--- src/main/java/com/web/service/OperLogService.java
+++ src/main/java/com/web/service/OperLogService.java
@@ -14,100 +14,57 @@ import com.web.example.OperLogExample;
  * @author 田军兴
  * @date 2016-06-29
  */
-public interface OperLogService extends IService<OperLog, String> {
+public interface OperLogService {
+	/**
+	 * @param operType
+	 *            操作类型(增删改查)
+	 * @param systemAction
+	 *            动作对象
+	 * @param operProp
+	 *            操作的属性（JSON字符串格式）
+	 */
+	public int addSystemLog(OperLog.operTypeEnum operType, OperLog.actionSystemEnum systemAction, String operProp);
 
 	/**
-	 * 数据操作类型 查询
+	 * @param operType
+	 *            操作类型(增删改查)
+	 * @param systemAction
+	 *            动作对象
+	 * @param operProp
+	 *            操作的属性（JSON字符串格式）
+	 * @param logLevel
+	 *            日记级别 成功或失败
 	 */
-	public static final String OPER_TYPE_SELECT = "select";
-	/**
-	 * 数据操作类型 插入
-	 */
-	public static final String OPER_TYPE_INSERT = "insert";
-	/**
-	 * 数据操作类型 修改
-	 */
-	public static final String OPER_TYPE_UPDATE = "update";
-	/**
-	 * 数据操作类型 删除
-	 */
-	public static final String OPER_TYPE_DELETE = "delete";
-
-	/**
-	 * 日志结果类型 成功
-	 */
-	public static final int LOG_LEVEL_SUCCESS = 0;
-	/**
-	 * 日志结果类型 失败
-	 */
-	public static final int LOG_LEVEL_ERROR = 1;
-
-	/**
-	 * 行为 创建机柜
-	 */
-	public static final int ACTION_ADD_CABINET = 1;
-	/**
-	 * 行为 创建设备
-	 */
-	public static final int ACTION_ADD_DEVICE = 2;
-	/**
-	 * 行为 创建跳线
-	 */
-	public static final int ACTION_ADD_JUMPER = 3;
-	/**
-	 * 行为 修改机柜属性
-	 */
-	public static final int ACTION_MODIFY_CABINET = 4;
-	/**
-	 * 行为 修改设备属性
-	 */
-	public static final int ACTION_MODIFY_DEVICE = 5;
-	/**
-	 * 行为 删除机柜
-	 */
-	public static final int ACTION_DELETE_CABINET = 6;
-	/**
-	 * 行为 删除设备
-	 */
-	public static final int ACTION_DELETE_DEVICE = 7;
-	/**
-	 * 行为 删除跳线
-	 */
-	public static final int ACTION_DELETE_JUMPER = 8;
-	/**
-	 * 行为 查询机柜
-	 */
-	public static final int ACTION_VIEW_CABINET = 9;
-	/**
-	 * 行为 查询设备
-	 */
-	public static final int ACTION_VIEW_DEVICE = 10;
+	public int addSystemLog(OperLog.operTypeEnum operType, OperLog.actionSystemEnum systemAction, String operProp,
+			OperLog.logLevelEnum logLevel);
 
 	/**
 	 * @param deviceName
 	 *            设备名称
 	 * @param operType
 	 *            操作类型(增删改查)
-	 * @param logLevel
-	 *            日志级别
-	 * @param action
-	 *            动作
+	 * @param busiAction
+	 *            动作对象
 	 * @param operProp
 	 *            操作的属性（JSON字符串格式）
-	 * @param userId
-	 *            操作人员ID
-	 * @param userName
-	 *            操作人姓名
-	 * @param operDate
-	 *            操作时间
-	 * @param comment
-	 *            备注
 	 */
-	public int addOperLog(String deviceName, String operType, int logLevel, int action, String operProp, String userId,
-			String userName, Date operDate, String comment);
+	public int addBusinessLog(String deviceName, OperLog.operTypeEnum operType, OperLog.actionBusinessEnum busiAction,
+			String operProp);
 
-	public int addOperLog(HttpServletRequest request, String deviceName, String operType, int logLevel, int action, String operProp,
-			String comment);
+	/**
+	 * @param deviceName
+	 *            设备名称
+	 * @param operType
+	 *            操作类型(增删改查)
+	 * @param busiAction
+	 *            动作对象
+	 * @param operProp
+	 *            操作的属性（JSON字符串格式）
+	 * @param logLevel
+	 *            日志级别 成功或失败
+	 */
+	public int addBusinessLog(String deviceName, OperLog.operTypeEnum operType, OperLog.actionBusinessEnum busiAction, String operProp,
+			OperLog.logLevelEnum logLevel);
 
 	/**
 	 * 分页查询日志
