56b4f52d98c2727c4c73aeb3d52f57fe39c14ceb
tianjx <tianstyle@163.com>
2016-08-14 19:42:35 +0800

    增加图片处理方法

--- src/main/java/com/web/action/ServiceRoomController.java
+++ src/main/java/com/web/action/ServiceRoomController.java
@@ -1,8 +1,11 @@
 package com.web.action;
 
+import java.io.File;
+import java.util.Date;
+import java.util.Iterator;
+
 import javax.servlet.http.HttpServletRequest;
 
-import com.web.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.alibaba.fastjson.JSON;
 import com.web.core.action.BaseController;
@@ -21,15 +27,9 @@ import com.web.entity.ServiceRoom;
 import com.web.example.ServiceRoomExample;
 import com.web.service.ServiceRoomService;
 import com.web.util.AllResult;
+import com.web.util.DateUtil;
 import com.web.util.StringUtil;
 import com.web.util.fastjson.FastjsonUtils;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
-import java.io.File;
-import java.util.Date;
-import java.util.Iterator;
 
 /**
  * 机房管理控制器
