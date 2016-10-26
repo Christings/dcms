864ec0139958af72cd01657c3daccdd8ea708191
tianjx <tianstyle@163.com>
2016-10-26 12:56:35 +0800

    增加文件校验

--- src/main/java/com/web/action/business/ServiceRoomController.java
+++ src/main/java/com/web/action/business/ServiceRoomController.java
@@ -59,6 +59,11 @@ public class ServiceRoomController extends BaseController {
 			}
 			ArrayList<FileUtilBean> files = FileUtil.uploadFiles(request, "upload/serviceRoom", false);// 上传文件
 			if (files.size() > 0) {
+				if (!"png".equalsIgnoreCase(files.get(0).getFileExt()) && !"jpg".equalsIgnoreCase(files.get(0).getFileExt())
+						&& !"jpeg".equalsIgnoreCase(files.get(0).getFileExt())) {
+					FileUtil.deleteFiles(files);
+					return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请上传正确的图片文件(PNG/JPG/JPEG)");
+				}
 				serviceRoom.setImageUrl(files.get(0).getFileRealPath());
 			}
 			serviceRoom.setId(UUIDGenerator.generatorRandomUUID());
@@ -102,6 +107,11 @@ public class ServiceRoomController extends BaseController {
 			}
 			ArrayList<FileUtilBean> files = FileUtil.uploadFiles(request, "upload/serviceRoom", false);// 上传文件
 			if (files.size() > 0) {
+				if (!"jpg".equalsIgnoreCase(files.get(0).getFileExt()) && !"png".equalsIgnoreCase(files.get(0).getFileExt())
+						&& !"jpeg".equalsIgnoreCase(files.get(0).getFileExt())) {
+					FileUtil.deleteFiles(files);
+					return AllResult.buildJSON(HttpStatus.BAD_REQUEST.value(), "请上传正确的图片文件(PNG/JPG/JPEG)");
+				}
 				serviceRoom.setImageUrl(files.get(0).getFileRealPath());
 			}
 			if (serviceRoomService.updateById(serviceRoom) > 0) {
