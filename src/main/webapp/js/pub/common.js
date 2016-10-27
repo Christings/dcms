5188121e7c72043482f5d45538f634653d7c4aa8
CharlesSong <charles2song@gmail.com>
2016-10-27 23:03:46 +0800

    超时时自动跳转到登陆界面

--- src/main/webapp/js/pub/common.js
+++ src/main/webapp/js/pub/common.js
@@ -3,6 +3,14 @@
  * 约定：common.js最先执行，其他页面每个页面必须丁一pageInit()方法，由common.js统一调用
  */
 $(document).ready(function () {
+    //先判断当前iframe页面的src与data-id是否一致,否则跳转到首页
+    loadTopWindow();
+    function loadTopWindow() {
+        if (window.top != null && document.URL.indexOf('login.html')!=-1) {
+            window.top.location = document.URL;
+        }
+    }
+
     var user=DCMSBusi.USER.get();
     if(!user){
         // window.location.href=DCMSUtils.URL.getContentPath()+"login.html";
